48c96a4841796302ef9ebe7e39b81dc25107bc2d
gudmundursvr <gudmundursvr@gmail.com>
2020-12-08 22:48:34 +0000

    hamburger menu on top noNav

--- src/components/Hamburger/index.jsx
+++ src/components/Hamburger/index.jsx
@@ -1,26 +1,86 @@
 import React from 'react';
+import {  } from '../../views/Cinemas/index';
 import {
-  Text, View, TouchableOpacity,
+  Text, View, TouchableOpacity, Modal
 } from 'react-native';
-// import { withNavigation } from 'react-navigation';
+import { withNavigation } from 'react-navigation';
+import PropTypes from 'prop-types';
 import { Ionicons } from '@expo/vector-icons';
 import styles from './styles';
 
-const Hamburger = () => (
-  <View
-    style={styles.HamburgerContainer}
-  >
-    <TouchableOpacity
-      activeOpacity={0.7}
-      onPress={() => alert('ekki reddy')}
+
+class Hamburger extends React.Component {
+  constructor(props) {
+    super(props);
+    // console.log(props)
+    this.state = {
+      menuIsOpen: false,
+    };
+  }
+
+
+    render() {
+
+  const {menuIsOpen} = this.state;
+  return (
+    <View
+      style={styles.HamburgerContainer}
+    >
+      <TouchableOpacity
+        activeOpacity={0.7}
+        onPress={() => this.setState({ menuIsOpen: !menuIsOpen })}
       >
+        <Ionicons name="ios-menu" style={styles.HamburgerIcon} />
+      </TouchableOpacity>
 
-      <Ionicons name="ios-menu"
-      style={styles.HamburgerIcon}
-     />
-    </TouchableOpacity>
-  </View>
-);
+      <Modal
+        visible={menuIsOpen}
+        transparent
+        animationType={'fade'}
+      >
+        <TouchableOpacity
+          style={styles.modalOver}
+          onPress={() => this.setState({ menuIsOpen: false })}
+        />
+        <View style={styles.menuPosition}>
+          <TouchableOpacity
+            activeOpacity={0.9}
+            style={styles.menuButton}
+            onPress={() => { this.setState({ menuIsOpen: false }); console.log('hohoh')}}
+          >
+            <Text> Kvikmyndahúsin </Text>
+          </TouchableOpacity>
 
+          <TouchableOpacity
+            activeOpacity={0.9}
+            style={styles.menuButton}
+            onPress={() => { this.setState({ menuIsOpen: false }); console.log('Það eru jólinn')}}
+          >
+            <Text> Myndirnar </Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            activeOpacity={0.9}
+            style={styles.menuButton}
+            onPress={() => this.setState({ menuIsOpen: false })}
+          >
+            <Text> Væntanlegar </Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            activeOpacity={0.9}
+            style={styles.menuButton}
+            onPress={() => this.setState({ menuIsOpen: false })}
+          >
+            <Text> something </Text>
+          </TouchableOpacity>
+        </View>
+
+      </Modal>
+    </View>
+    );
+}
+}
 
 export default Hamburger;
+//this.props.navigation.navigate('Gummistest')
--- src/components/Hamburger/styles.js
+++ src/components/Hamburger/styles.js
@@ -2,16 +2,40 @@ import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
   HamburgerContainer: {
-    position: 'absolute',
-    top:40,
-    right: 10,
-
-
+    position: 'absolute', // put the menu on top and over the navigation
+    top: 16,
+    right: 0,
   },
 
-
   HamburgerIcon: {
+    padding: 20, // using this so clicable area are biggers
     fontSize: 30,
     color: '#fff',
   },
+
+
+  modalOver: {
+    width: '100%',
+    height: '100%',
+    // backgroundColor: '#000',
+    // opacity: 0.7,
+  },
+
+  menuPosition:{
+    position: 'absolute', // put the menu on top and over the navigation
+    top: 50,
+    right: 0,
+  },
+  menuButton: {
+    marginBottom: 6,
+    backgroundColor: '#fff',
+    width: 200,
+    height: 60,
+    padding: 20,
+    alignItems: 'center',
+    borderTopLeftRadius: 20,
+    borderBottomLeftRadius: 20,
+
+  },
+
 });
