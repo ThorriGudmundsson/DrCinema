954ba9c60a818999a413c9bf29716f9ed8566181
Berglindav <berglindav@gmail.com>
2020-12-09 15:34:02 +0000

    fixing cinemaDetail wiew

--- src/views/CinemaDetail/index.jsx
+++ src/views/CinemaDetail/index.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+// import {  } from '../UpcomingMovies/index';
 import {
   View, Text,
 } from 'react-native';
 // import { NavigationEvents } from 'react-navigation';
 import { connect } from 'react-redux';
+import { selectCinema } from '../../actions/cinemaActions';
+import { getMoviesInCinema } from '../../actions/movieActions';
 import { getCinemaById } from '../../services/cinemaService';
-import CinemaDetails from '../../components/CinemaDetail/index';
+import * as movieService from '../../services/movieService';
+import Hamburger from '../../components/Hamburger';
 
 class CinemaDetail extends React.Component {
   constructor(props) {
@@ -13,30 +17,42 @@ class CinemaDetail extends React.Component {
     // console.log(props);
     this.state = {
       cinema: {},
+      movies: [],
     };
     // console.log(this.props);
   }
 
   async componentDidMount() {
+    const { selectCinema, getMoviesInCinema } = this.props;
     const cinema = await getCinemaById(this.props.navigation.state.params.id);
+    // console.log(cinema.id);
+    const movies = await movieService.getAllMoviesByCinemaId(cinema.id);
     this.setState({
       cinema,
+      movies,
     });
+
+    selectCinema(
+      cinema['address\t'],
+      cinema.city,
+      cinema.description,
+      cinema.google_map,
+      cinema.id,
+      cinema.name,
+      cinema.phone,
+      cinema.website,
+    );
+    getMoviesInCinema(cinema.id);
   }
 
   render() {
-    const { cinema } = this.state;
-    console.log(cinema)
+    const { cinema, movies } = this.state;
     return (
       <View>
         <Text>{cinema.name}</Text>
-        <Text>{cinema.description}</Text>
-        <Text>{cinema.address}</Text>
-        <Text>{cinema.phone}</Text>
-        <Text>{cinema.website}</Text>
-
-        <CinemaDetails
-          cinemas={cinema}
+        <Hamburger
+          navigation={this.props.navigation}
+          themecolor='#333'
         />
       </View>
     );
@@ -45,4 +61,4 @@ class CinemaDetail extends React.Component {
 
 // const mapStateToProps = ({ cinema }) => ({ cinema });
 
-export default connect(null, { getCinemaById })(CinemaDetail);
+export default connect(null, { selectCinema, getMoviesInCinema })(CinemaDetail);
