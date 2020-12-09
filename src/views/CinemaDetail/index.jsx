822719b46f9a426132e073bbef388fd34024b179
Berglindav <berglindav@gmail.com>
2020-12-09 15:49:43 +0000

    cinemaDetail view added description, addres, phone etc.

--- src/views/CinemaDetail/index.jsx
+++ src/views/CinemaDetail/index.jsx
@@ -50,6 +50,10 @@ class CinemaDetail extends React.Component {
     return (
       <View>
         <Text>{cinema.name}</Text>
+        <Text>{cinema.description}</Text>
+        <Text>{cinema.address}</Text>
+        <Text>{cinema.phone}</Text>
+        <Text>{cinema.website}</Text>
         <Hamburger
           navigation={this.props.navigation}
           themecolor='#333'
